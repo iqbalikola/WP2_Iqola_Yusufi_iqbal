520cff7f068f0f8b812f798448aa3bf1907a924f
iqbalikola <90953588+iqbalikola@users.noreply.github.com>
2021-10-19 09:28:00 +0700

    Add files via upload
    Tugas web 2

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Iqola Yusufi iqbal
+        Saya tingga di daerah Petukangan selatan
+        hobi yang saya sukai adalah
+        Menggambar";
+    }
+}
